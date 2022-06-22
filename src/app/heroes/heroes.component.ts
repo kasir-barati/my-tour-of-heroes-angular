85e8cb99d85fb60acaf7518cd66d74675e0d8f78
Kasir <kasir.barati@gmail.com>
2022-06-22 08:03:05 +0430

    add: hero service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,16 +9,34 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  heroes: Hero[] = HEROES;
+  heroes: Hero[];
   // Please note that mark nullable parameters as nullable
   selectedHero?: Hero;
 
-  constructor() {}
+  constructor(private heroService: HeroService) {
+    /**
+     * Reserve the constructor for:
+     *   - minimal initialization - e.x. wiring constructor parameters to properties.
+     *   - The constructor shouldn't do anything.
+     *   - It certainly shouldn't call a function that makes HTTP requests.
+     */
+    this.heroes = [];
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    /**
+     * Angular calls ngOnInit() at an appropriate time after constructing a HeroesComponent instance.
+     * So ngOnInit lifecycle hook is a better place to do a HTTP request.
+     */
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     // Because I guess we are using pass by reference changes in the selectedHero goes all the way up to reach to the heroes property.
     this.selectedHero = hero;
+  }
+
+  private getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 }
